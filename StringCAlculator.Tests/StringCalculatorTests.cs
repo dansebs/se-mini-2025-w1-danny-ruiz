f1eb2fc8608a816ef011baf774be066c2e2b70e5
ruizd <ruizd@P30411>
2025-03-11 08:59:23 +0100

    Add project files.

---
+++ StringCAlculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+public class StringCalculatorTests
+{
+    [Fact]
+    public void Given_EmptyString_When_CalculatingSum_Then_returnZero()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        int result = calculator.Add("");
+        //Then
+        Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Given_SingleNumber_When_CalculatingSum_Then_Return_That_Number()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When 
+        int result = calculator.Add("5");
+        //Then
+        Assert.Equal(5, result);
+    }
+    [Fact]
+    public void Given_CommaDelimited_SUM()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        int result = calculator.Add("5,5");
+        //Then
+        Assert.Equal(10, result);
+    }
+    [Fact]
+    public void Given_newline_SUM()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        int result = calculator.Add("10\n10");
+        //Then
+        Assert.Equal(20, result);
+    }
+    [Fact]
+    public void Given_3_numbers_Any_Delimiter()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        int result = calculator.Add("10,10\n10");
+        //Then
+        Assert.Equal(30, result);
+    }
+    [Fact]
+    public void Given_negative_throw_exception()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        //Then
+        Assert.Throws<Exception>(() => calculator.Add("-100"));
+    }
+    [Fact]
+    public void Given_num_greater_than_thousand()
+    {
+        //Given
+        var calculator = new StringCalculator();
+        //When
+        int result = calculator.Add("1001,100,10");
+        //Then
+        Assert.Equal(110,result);
+    }
+}
+
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        List<char> list = new List<char>();
+
+        list.Add('\n');list.Add(',');
+
+
+        if (numbers.Contains('-')) throw new Exception("Exist negative number");
+
+        if (numbers.Contains('\n') || numbers.Contains(','))
+        {
+            string[] result = numbers.Split(',');
+            int sum = 0;
+            foreach (string number in result)
+            {
+                if (number.Contains("\n"))
+                {
+                    string[] nums = number.Split("\n");
+                    foreach (string num in nums)
+                    {
+                        sum += int.Parse(num) > 1000 ? 0 : int.Parse(num); ;
+                    }
+                }
+                else
+                {
+                    sum += int.Parse(number) > 1000 ? 0 : int.Parse(number); ;
+                }
+            }
+            return sum;
+        }
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+        int rt = int.Parse(numbers) > 1000 ? 0:int.Parse(numbers);
+        return rt;
+    }
+
+
+}
+
